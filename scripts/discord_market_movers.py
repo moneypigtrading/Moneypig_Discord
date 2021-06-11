ead1ab96b41bf4c718ec7ad78a8530638e9c70a8
Pin-Chih Su <pin-chihsu@Pin-Chihs-MBP.webpass.net>
2021-06-11 08:30:32 -0700

    fix the percentage and add the most active

--- scripts/discord_market_movers.py
+++ scripts/discord_market_movers.py
@@ -68,6 +68,12 @@ class discord_market_movers():
 
         gainers_df=gainers_df[['latestPrice','changePercent','companyName']]
 
+        gainers_df['latestPrice']=gainers_df['latestPrice'].round(2)
+
+        gainers_df['changePercent']=gainers_df['changePercent'].round(2)
+
+        gainers_df['changePercent']=gainers_df['changePercent']*100
+
         gainers_df.to_csv(gainers_csv)
 
         d.send_format_csv_to_discord('Market Movers', gainers_csv,'gainers_df_reformat.csv',  self.webhook_id, self.webhook_token)
@@ -82,12 +88,44 @@ class discord_market_movers():
 
         losers_df=losers_df[['latestPrice','changePercent','companyName']]
 
+        losers_df['latestPrice']=losers_df['latestPrice'].round(2)
+
+        losers_df['changePercent']=losers_df['changePercent'].round(2)
+
+        losers_df['changePercent']=losers_df['changePercent']*100
+
+        # losers_df['changePercent']=losers_df['changePercent']*100
+
         losers_df.to_csv(losers_csv)
 
         d.send_format_csv_to_discord('Market Losers', losers_csv,'gainers_df_reformat.csv',self.webhook_id, self.webhook_token)
 
+
+        active_df=self.get_market_most_active(secret)
+
+        # print(gainers_df[['companyName','latestPrice','changePercent','marketCap','peRatio','week52High','week52Low','ytdChange']])
+
+        #'marketCap','peRatio','week52High','week52Low','ytdChange',
+
+        active_df=active_df[['latestPrice','changePercent','companyName']]
+
+        active_df['latestPrice']=active_df['latestPrice'].round(2)
+
+        active_df['changePercent']=active_df['changePercent'].round(2)
+
+        active_df['changePercent']=active_df['changePercent']*100
+
+        print(active_df.info())
+
+        active_df.to_csv(active_csv)
+
+        d.send_format_csv_to_discord('Market Most Active', active_csv,'active_df_reformat.csv',self.webhook_id, self.webhook_token)
+
+
         shutil.move(gainers_csv, "data/")
 
         shutil.move(losers_csv, "data/")
+
+        shutil.move(active_csv, "data/")
 
 
